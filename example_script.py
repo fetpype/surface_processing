a123cf6827c46257245bbc8ac734450002e8fea0
gauzias <guillaume.auzias@univ-amu.fr>
2025-07-31 14:59:09 +0200

    [WIP] create commandline and make the binary mask extraction more generic

--- example_script.py
+++ example_script.py
@@ -1,40 +1,18 @@
-import os
-
-import tempfile
-import numpy as np
-import nibabel as nib
-import util.generate_mesh as ugm
+from generate_mesh import mesh_extraction
 
 if __name__ == "__main__":
     # parameters to be set by the user
-    path_pymesh_container = "/scratch/gauzias/softs/pymesh_latest.sif"
-    lut_file = "/scratch/gauzias/code_gui/surface_processing/util/critical186LUT.raw.gz"
-    nb_iter = 20
-    dt = 0.3
     input_bounti_seg_mask = "/scratch/gauzias/data/datasets/MarsFet/output/svrtk_BOUNTI/output_BOUNTI_seg/haste/sub-0009/ses-0012/reo-SVR-output-brain-mask-brain_bounti-19.nii.gz"
-    output_mesh_basename = "/scratch/gauzias/data/test_surface_processing/sub-0009_ses-0012_reo-SVR-output-brain-mask-brain_bounti-19"
-
-    print(input_bounti_seg_mask)
-    mask_filename = os.path.basename(input_bounti_seg_mask)
-    # get WM hemi masks
-    nii_white_hemi_mask = ugm.extract_hemi_mask_bounti(input_bounti_seg_mask)
-    data_mask = nii_white_hemi_mask.get_fdata()
-    for label,side in zip([1,2],['left','right']):
-        # label 1 = Left hemisphere
-        # label 2 = Right hemisphere
-        output_mesh = output_mesh_basename+"."+side+".surf.gii"
-        with tempfile.NamedTemporaryFile(suffix="."+side+".hemi_mask.nii.gz") as temp_mask:
-
-            data_hemi = np.zeros_like(data_mask)
-            data_hemi[data_mask==label] = 1
-            nii_tmpfile_hemi = nib.Nifti1Image(data_hemi, nii_white_hemi_mask.affine)
-            nib.save(nii_tmpfile_hemi, temp_mask.name)
-
-            ugm.generate_mesh(
-                lut_file,
-                temp_mask.name,
-                output_mesh,
-                path_pymesh_container,
-                nb_iter,
-                dt,
-            )
+    output_mesh = "/scratch/gauzias/data/test_surface_processing/sub-0009_ses-0012_reo-SVR-output-brain-mask-brain_bounti-19.left.white.gii"
+    # list of labels to concatenate in order to get the white hemi mask
+    concatenated_labels = [5,7,14,16]# for left
+    #concatenated_labels = [6,8,15,17]# for right
+    nb_smoothing_iter = 20
+    smoothing_step = 0.3
+    mesh_extraction(
+        input_bounti_seg_mask,
+        concatenated_labels,
+        output_mesh,
+        nb_smoothing_iter,
+        smoothing_step,
+    )
